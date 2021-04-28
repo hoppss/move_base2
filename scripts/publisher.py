12b344e1fae6c8c85b04358e79f23ffa70e4e306
李茂 <limao1@xiaomi.com>
2021-04-28 19:08:08 +0800

    test with nav2_receive_goal

---
+++ scripts/publisher.py
@@ -0,0 +1,48 @@
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import PoseStamped
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(PoseStamped, '/tracking_pose', 10)
+        #self.publisher_ = self.create_publisher(PoseStamped, '/goal_pose', 10)
+        timer_period = 1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = PoseStamped()
+        msg.header.frame_id = 'map'
+        msg.pose.position.x = 1.0
+        msg.pose.position.y = -0.5
+        msg.pose.position.z = 0.0
+        msg.pose.orientation.x = 0.0
+        msg.pose.orientation.y = 0.0
+        msg.pose.orientation.z = 0.0
+        msg.pose.orientation.w = 1.0
+        self.publisher_.publish(msg)
+        #self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+        self.timer.cancel()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
