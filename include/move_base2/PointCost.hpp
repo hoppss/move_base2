ebdf70e13f19cb733f33bf4a3552bac2b1d116a3
李茂 <limao1@xiaomi.com>
2021-08-05 22:23:30 +0800

    add license in header

--- include/move_base2/PointCost.hpp
+++ include/move_base2/PointCost.hpp
@@ -1,3 +1,16 @@
+// Copyright 2021  Xiaomi Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #ifndef POINTCOST_H_
 #define POINTCOST_H_
 
@@ -23,7 +36,7 @@ public:
   void initialize(const nav2_util::LifecycleNode::SharedPtr& nh,
                   std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros);
 
-  bool isValidPose (const geometry_msgs::msg::PoseStamped &p, bool allow_unknown = true);
+  bool isValidPose(const geometry_msgs::msg::PoseStamped& p, bool allow_unknown = true);
   unsigned char getPointCost(const geometry_msgs::msg::PoseStamped& p);
 
   bool collisionFreeCheck(const nav_msgs::msg::Path& path, double& sum_dist);  // false is collision
